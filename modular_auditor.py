b3c3a98d93d6f7eba797c7065e2387bb0f6df00e
Zachary Yong <ydzach@gmail.com>
2026-09-18 09:40:07 +0800

    lab 3.2

--- modular_auditor.py
+++ modular_auditor.py
@@ -2,19 +2,24 @@ inventory = 0
 stock = ("")
 error = 0
 
+def get_valid_input(inv):
+    if inv.isdigit() or inv == ("quit"):
+        return inv
+
+def process_delivery(current_total, new_total):
+    if current_total.isdigit():
+        new_total+=int(current_total)
+        return new_total
+
+
+
 while stock != ("quit"):
     stock = (input("Enter value: "))
+    get_valid_input(stock)
+    process_delivery(stock, inventory)
 
-    if stock.isdigit():
-        inventory += int(stock)
 
-        if inventory >500:
-             print ("Too Full!")
-             break
-
-    elif stock !=("quit"):
-        print("Rejected")
-        error +=1
+    
 
 print("Total Unit Processed: ", inventory)
 print("Rejected Entry: ", error) 
